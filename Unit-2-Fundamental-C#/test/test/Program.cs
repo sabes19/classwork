2460b2c13b8ceed750930b01a576e8bf74f12ea5
Ryan Sabon <ryansabon@wayne.com>
2024-08-12 18:44:42 -0400

    message

---
+++ Unit-2-Fundamental-C#/test/test/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+namespace test
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            // ask for the users age
+            Console.WriteLine("Please enter your age: ");
+            string input = Console.ReadLine();
+
+            // validate the age falls within a set range
+            if (int.TryParse(input, out int age))
+            {
+                if (age < 0 || age > 100)
+                {
+                    Console.WriteLine("Invalid age entered. Please enter a valid age between 0 and 100.");
+                }
+                else if (age >= 21)
+                {
+                    Console.WriteLine("You are legally allowed to drink.");
+                }
+                else
+                {
+                    Console.WriteLine("You are not legally allowed to drink.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid number for age.");
+            }
+        }
+    }
+}
+
