c9f6e7bae09293703c6a5e061dbfc73ab3a6f183
Ryan Sabon <ryansabon@wayne.com>
2024-08-14 18:39:01 -0400

    message

---
+++ Unit-2-Fundamental-C#/Basic-Loops-Numbers/Basic-Loops-Numbers/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string userInput;
+
+        do
+        {
+            Console.Write("Enter a number: ");
+            if (int.TryParse(Console.ReadLine(), out int number))
+            {
+                // Loop to output numbers from 'number' to 0
+                for (int i = number; i >= 0; i--)
+                {
+                    Console.Write(i + " ");
+                }
+                Console.WriteLine();
+
+                // Loop to output numbers from 0 to 'number'
+                for (int i = 0; i <= number; i++)
+                {
+                    Console.Write(i + " ");
+                }
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number.");
+            }
+
+            Console.Write("Would you like to continue (y/n)? ");
+            userInput = Console.ReadLine().Trim().ToLower();
+
+        } while (userInput == "y");
+
+        Console.WriteLine("Goodbye!");
+    }
+}
