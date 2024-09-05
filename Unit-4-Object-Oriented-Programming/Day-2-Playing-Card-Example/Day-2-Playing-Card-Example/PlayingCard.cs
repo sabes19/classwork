d69ec28850d108bb1db835e9069c1befe1e1a41c
Ryan Sabon <ryansabon@wayne.com>
2024-09-05 18:29:19 -0400

    hello

---
+++ Unit-4-Object-Oriented-Programming/Day-2-Playing-Card-Example/Day-2-Playing-Card-Example/PlayingCard.cs
@@ -0,0 +1,116 @@
+﻿namespace Day_2_Playing_Card_Example
+{
+    // this is the class to represent a simple Playing card
+    // its in the same namespace as the application (for now and for simplicity
+    //
+    // access - public - anyone can access and use
+    //        - private - only members of this class can access and use
+    // 
+    //
+    public class PlayingCard
+//  access type  name
+    {
+        /********************************************************
+        * Data Members (attributes, properties, variables)
+        *********************************************************/
+
+        // define instance variables for the class (unique attributes for each object)
+
+        
+
+        // define instance variables for the class (unique attributes for each object)
+        //
+        // instance variables are private to implement encapsulation
+
+        private int cardValue;
+        private string cardSuit;
+        private string cardColor;
+
+        // to provide access so others can "see" or "change" the data members
+        //      we can provide "properties" to do that
+        
+        // a property is a public variable to allow access to private data
+        //   using "getters" and "setters"
+        //
+        // a Getter will return the value in a data member
+        // a setter will allow the changing of a data member
+        
+        // a property is typically named the same as the variable in PascalCase
+
+        // Define properties for access to data members 
+        public int CardValue // name is the data member in PascalCase
+        {
+            get {return cardValue;}  // getter - return the value in cardValue
+            set {cardValue = value;} // setter - set cardValue to value used when assigning 
+//                                     //          value is keyword representing the value
+        }
+
+
+
+
+
+
+        /********************************************************
+        * Method members (functions that operate on that class data) 
+        *********************************************************/
+
+        // every class has at least on constructor
+        //
+        // constructors are responsible for initailizing that data in the object
+        // constructors are public, same name as the class, no return type, may have parameters
+
+        // constuctors to initialize all instance variables
+        public PlayingCard(int theValue, string theSuit, string theColor)
+        {
+            cardColor = theColor; // initialize color to color passed
+            cardValue = theValue; // initialize value to value passed
+            cardSuit  = theSuit;  // initialize suit to suit passed
+        }
+
+        // a copy constructor to create a copy of PlayingCard from a PlayingCard
+        // (aka a "deep copy")
+        // this avoids two references to the same object wwhen creating an object from an object
+        //
+        // a copy constructor receives  an object of thje class as a parameter
+        // and initializes using the data memebers in that object
+
+
+        public PlayingCard(PlayingCard sourceCard)
+        {
+            cardColor = sourceCard.cardColor; // initialize color to color passed
+            cardSuit  = sourceCard.cardSuit;  // initialize suit to suit passed
+            cardValue = sourceCard.cardValue; // initialize value to value passed
+        }
+
+        /******************************************************************************
+        * Method overrides to have classs behave the way we want, not the defualt way
+        ******************************************************************************/
+        
+        // Override the defualt ToString() method
+        // we MUST be sure the method signiture matches the method we are overriding
+        // an override is substituting your processing for the defualt processing
+        // good idea to autofill
+
+        // since the default ToString() returns a string and receives no parameters
+        // the override must do the same
+
+        // the override key work, tells C# this is an override for default behavior
+        //                         C# will check to be sure your override signiture
+        //                                                  matches the defualt
+
+        // a method signiture identifies the method and contains
+        //
+        // 1. return type
+        // 2. a method name
+        // 3. parameter
+
+        // the purpose of ToString() is to present an object of the class as a string
+        public override string ToString() 
+        {
+            return $"PlayingCard: Value={cardValue}, Color={cardColor}, Suit={cardSuit}";
+        }
+
+
+
+    } // end of PlayingCard class
+} // End of namespace
--- Unit-4-Object-Oriented-Programming/Day-2-Playing-Card-Example/Day-2-Playing-Card-Example/Program.cs
+++ Unit-4-Object-Oriented-Programming/Day-2-Playing-Card-Example/Day-2-Playing-Card-Example/Program.cs
@@ -1,15 +1,70 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using GeneralPurposeFunctions; // gives me access to general purpose function code
 
 namespace Day_2_Playing_Card_Example
 {
     internal class Program
     {
+            // Instatiate a cop[y of the code in CommonlyUsedFunctions
+            //          Called myFuncs
+            // static bc its used in Main() which is static
+        static CommonlyUsedFunctions myFuncs = new CommonlyUsedFunctions();
+
+        // this is the application program
+        // 
+        // It will instantiate and manipulate objects of various classes
+
         static void Main(string[] args)
         {
+            myFuncs.WriteSeparatorLine("Welcome to our first OOP Example!");
+
+
+            /**********************************************************************
+            * Define/Instantiate a PlayingCard - an Ace of Hearts (red)
+            **********************************************************************/
+            myFuncs.WriteSeparatorLine("Instantiate and display a PlayingCard");
+
+        //  data type   name  = new data type  (initializers)
+            PlayingCard aCard = new PlayingCard(1, "Hearts", "Red");
+
+            Console.WriteLine($"aCard is: {aCard}"); // display the PlayingCard
+
+            //      WHY 2-STRING METHOD IS REQUIRED
+            // Console.WriteLine expects a string to be written
+            // interpolation ($"") - generates a string
+            // aCard is aPlayingCard type not a string
+            // in order for it to be written it needs to be converted to a string
+            // The default ToString() method to convert an object to a string generates
+            //              a string containing the "namesspace.className"
+            // If you want your class objects to be represented as a string,
+            //      you must override the defualt ToString() method in your class
+            // i.e. YOU decide what a string representation of your class looks like
+
+
+            /**********************************************************************
+            * Define/Instantiate a PlayingCard to be the same as aCard
+            *   and display them both
+            **********************************************************************/
+
+
+
+            //  PlayingCard newCard = aCard; // this makes both newCard and aCard point to the same object
+            PlayingCard newCard = new PlayingCard(aCard); // use copy constructor
+
+            Console.WriteLine($"  aCard is: {aCard}");
+            Console.WriteLine($"newCard is: {newCard}");
+
+
+            /**********************************************************************
+            * Change the value in newCard to be a two 
+            **********************************************************************/
+            myFuncs.WriteSeparatorLine("Change value in newCard to 2");
+        //  newCard.cardValue;  // cannot access private data in an object
+            newCard.CardValue = 2; // Use Property to change the value
+            Console.WriteLine($"newCard is: {newCard}");
+
+
+            myFuncs.PauseProgram();
         }
     }
 }
