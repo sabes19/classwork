d09e2a0d07f67e2b062ffe3754d11c49069a87c8
Ryan Sabon <ryansabon@wayne.com>
2024-09-23 18:35:53 -0400

    9/23

--- Unit-5-Unit-Testing/Day-3-File-IO/Day-3-File-IO/Program.cs
+++ Unit-5-Unit-Testing/Day-3-File-IO/Day-3-File-IO/Program.cs
@@ -9,10 +9,68 @@ namespace Day_3_File_IO
         {
             _myFuncs.WriteSeparatorLine("Welcome to a File I/O Example");
 
+            // Display the current execution folder (folder app is running from)
+            // The enviorment object gives yuou access to information about the current exedcution enviorment
+            Console.WriteLine($"Current Execution Folder is: {Environment.CurrentDirectory}");
 
-            _myFuncs.PauseProgram();
-            _myFuncs.WriteSeparatorLine("Thanks for visiting a File I/O Example");
+            // Display all the lines in the file numbers.txt in files folder/directory
 
+            // Define a StreamReader for the file path containing the file
+            // the files path is 3-levels up from our execution path
+            // ../ means to go up 1 level
+
+            // StreamReader allen = new StreamReader("../../../files/numbers.txt");
+
+            // Define a running total outside the loop
+            int runningTotal = 0;
+            try
+            {
+                using (StreamReader allen = new StreamReader("../../../files/numbers.txt"))
+
+                    // Loop until all the lines have been processed (not EndOfStream)
+                    while (!allen.EndOfStream)  // .EndOfStream will indicate when the file is empty (no more data)
+                    {
+                        //      Get a line from the file
+                        string line = allen.ReadLine();
+
+                        // Display and sum the numbers
+                        string[] theValues = line.Split(","); // Split line up into values
+
+                        int sum = 0; // reset the sum before we start adding all the numbers
+
+                        for (int i = 0; i < theValues.Length; i++)          // Loop through the values
+                        {
+
+                            string aValue = theValues[i].Trim();            // Trim any leading or trailing spaces from the value
+                            int intValue = int.Parse(aValue);               // Convert a value from string to int
+                                                                            //  int intValue = int.Parse(theValues[i].Trim());  // Alternate to two statements above
+
+                            sum += intValue;                                // add the int value to a sum
+
+                            runningTotal += sum;                            // add the sum to running total
+
+                            Console.WriteLine($"Value #{i}: {aValue}");     // Display the value
+                        }
+
+                        // After processing all the numbers
+
+                        Console.WriteLine($"Sum is: {sum}");
+                        Console.WriteLine($"Running subtotal: {runningTotal}");
+
+                        // Display the line
+                        Console.WriteLine(line);
+                        allen.Close();
+
+                    }
+
+
+
+                // Close the file when done to free up memory used to support the files
+
+                StreamWriter.Close();
+                _myFuncs.PauseProgram();
+                _myFuncs.WriteSeparatorLine("Thanks for visiting a File I/O Example");
+            }
         }
     }
 }
---
+++ Unit-5-Unit-Testing/Day-3-Logging-Example-Ryan/Day-3-Logging-Example-Ryan/Program.cs
@@ -0,0 +1,122 @@
+﻿using GeneralPurposeFunctions;
+using System.IO.Enumeration;
+
+namespace Day_3_Logging_Example_Ryan
+{
+    internal class Program
+    {
+        // by defining this outside any method, it is available to all methods in the class
+        // static is required because the methods will be used inside main, which is static
+        static CommonlyUsedFunctions myFuncs = new CommonlyUsedFunctions();
+
+        static string inputFilePath = "../../../data/IMDB-Top-25-SciFi-Movies.txt";
+
+
+        // To avoid trying to define the logFile mroe than once...
+        // Make the reference to the file static and outside any method
+        //      1. Defines one refernce
+        //      2. 
+
+        static StreamWriter logFile = null;                                // Define reference and set to null
+
+        static void Main(string[] args)
+        {
+            // Log the program start
+            LogEvent("Program Started...");
+
+
+            // Read and display all the data in the IMDB-Top-SciFi-Movies file
+
+            
+            StreamReader fileReader = new StreamReader(inputFilePath);              // Define a StreamReader for the file
+
+            
+            List<string> sciFiMovies = new List<string>();                          // Define a list to hold each line from the file
+
+
+
+
+
+
+
+
+
+
+            /**********************************************************
+            * Lets be sure we can read all the data successfully
+            **********************************************************/
+
+            //////////////////////////////////
+            //
+            //////////////////////////////////
+
+            // while-loop
+            while (!fileReader.EndOfStream)                 // loops as long as its not the end of the stream
+            {
+                string line = fileReader.ReadLine();        // Get a line from the file
+                sciFiMovies.Add(line);                      // Add a line to the list
+            }
+
+            // Loop until the user indicates they are done by typing 'N' or "No" when asked
+            while (myFuncs.moreInput())                      // moreInput() returns true if the user says they have more input...
+            {
+
+
+
+
+
+
+
+                // Ask the user which movie they want from the list by partial word search
+                // Prompt the user to entert a word to search for
+                Console.WriteLine("Please enter a word from the title of the movie you are searching for...");
+                // define a place to hold the user response (user's choice)
+                string userInput = Console.ReadLine();          // get a word from the user
+
+                // Check to see if any movies in the list contain words
+                // loop through the list of movies to see if the title contains the word
+                foreach (string aLine in sciFiMovies)
+                {
+                    if (aLine.Contains(userInput))
+                    {
+                        LogEvent($"Found title {userInput}");
+                        Console.WriteLine(aLine);
+                    } // ends the foreach
+                } // ends the outer while
+
+
+                fileReader.Close();                             // Release memory used by supporting data structures
+                
+                LogEvent("Program Ended...");
+
+
+                myFuncs.PauseProgram();
+            }
+
+
+        } // ends the Main()
+
+        /*******************************
+        // Helper methods for Main()
+        ********************************/
+        // Method: log program events including user interactions to a disk file
+        public static void LogEvent(string messageToLog)
+        {
+            // To avoid trying to define the logFile more than once...
+            
+
+
+            if (logFile == null)
+            {
+                // Define the disk file to hold the log
+                logFile = new StreamWriter("../../../program.log");
+            }
+        // Add a timestamp to the log message
+        // get the current timestamp as a string
+            DateTime currentTimestamp = DateTime.Now;       // get current time stamp
+            string formattedTime = currentTimestamp.ToString("yyyy-MM-dd-HH:mm:ss");
+            logFile.WriteLine($"{formattedTime} - {messageToLog}");
+            logFile.Flush();
+        }
+    } // ends the class with Main()
+} // end the namespace
