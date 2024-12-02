dffa989b24d974aee1335d55ed8e95ed0d46e440
Frank Fella <Frank@CyberAcademy.com>
2024-12-02 16:34:52 -0700

    Instructor Push 12/02/2024 16:34:51

---
+++ Unit-8-Backend-REST APIs/GamblerAPI-EF-Example/GamblerAPI/Model/Gambler.cs
@@ -0,0 +1,72 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace GamblerAPI.Model
+{
+    public class Gambler
+    {
+        /***********************************************************************
+         * Gambler Data Model for use with Entity Fraework
+         * 
+         * Describes data being transfered to and from the Server
+         ***********************************************************************/
+
+        // Data members with Entity Framework notations
+        // Use of [JsonProperty] is required when the class data member name
+        //       does not match the associated table column names
+
+        [JsonPropertyName("id")]                 // Relate JSON property to object property since names differ
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]  // This is a Generated value - Identity column
+        public int      Id        { get; set; }
+
+        [JsonPropertyName("gambler_name")]       // Relate JSON property to object property since names differ
+        public string   Name      { get; set; }
+
+        [JsonPropertyName("address")]             // Relate JSON property to object property since names differ
+        public string?  Address   { get; set; }
+
+        [JsonPropertyName("monthly_salary")]      // Relate JSON property to object property since names differ
+        public double   Salary    { get; set; }
+
+        [JsonPropertyName("birth_date")]          // Relate JSON property to object property since names differ
+        public DateTime BirthDate { get; set; }
+
+
+        // 0-arg constructor may be used by C# when needed
+        public Gambler() { }
+
+        // 5-arg constructor
+        public Gambler(int id, string name, string address, double salary, DateTime birthDate)
+        {
+            Id = id;
+            Name = name;
+            Address = address;
+            Salary = salary;
+            BirthDate = birthDate;
+        }
+
+        /**************************************************************************************************
+         * These overrides are not required for Entity Framework, but may also be found in a class using
+         *       Entity Framework
+         **************************************************************************************************/ 
+        public override bool Equals(object? obj)
+        {
+            return obj is Gambler gambler &&
+                   Id == gambler.Id &&
+                   Name == gambler.Name &&
+                   Address == gambler.Address &&
+                   Salary == gambler.Salary &&
+                   BirthDate == gambler.BirthDate;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string? ToString()
+        {
+            return base.ToString();
+        }
+    } // End of class
+} // End of namespace
---
+++ Unit-8-Backend-REST APIs/GamblerAPI-EF-Example/GamblerAPI/Model/GamblerDbContext.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GamblerAPI.Model
+{
+    public partial class GamblerDbContext : DbContext
+    {
+        // Define constructror to take options passed when instatiated by application
+        //        and pass those options to base (DbContext) cclass
+        public GamblerDbContext(DbContextOptions<GamblerDbContext> options)
+            : base(options) { }
+
+        // Define property to represent the collection of data in the data source
+        public virtual DbSet<Gambler> Gamblers { get; set; }
+
+        // Define table and columns to be accessed using Entity Framework
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Gambler>(entity =>
+            {
+                entity.ToTable("gambler");
+
+                entity.Property(e => e.Id)
+                    .HasColumnName("id")
+                    .IsRequired()
+                    .HasColumnType("smallint");
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasColumnName("gambler_name")
+                    .HasColumnType("text");
+
+                entity.Property(e => e.Address)
+                    .HasColumnName("address")
+                    .HasColumnType("text")
+                    .HasDefaultValue("unknown");
+
+                entity.Property(e => e.BirthDate)
+                      .IsRequired()
+                      .HasColumnName("birth_date")
+                      .HasColumnType("date");
+
+                entity.Property(e => e.Salary)
+                    .IsRequired()
+                    .HasColumnName("monthly_salary")
+                    .HasColumnType("decimal(9,2)");
+
+                OnModelCreatingPartial(modelBuilder);
+            });  // End of modelBuilder.entity()
+        }  // End of onModelCreating()
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+        
+    } // End of class
+} // End of namespace
