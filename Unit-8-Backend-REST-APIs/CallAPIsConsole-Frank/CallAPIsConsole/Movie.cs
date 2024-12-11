958c6ab7645a98862cd26e20e664000eedc4d61a
FFrank Fella <Frank@CyberAcademy.com>
2024-12-11 11:31:56 -0700

    Instructor Push 12/11/2024 11:31:56

---
+++ Unit-8-Backend-REST-APIs/CallAPIsConsole-Frank/CallAPIsConsole/Movie.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+/*******************************************************************************
+ * This is the data we are sending and receiving to/from the API server
+ * 
+ * We mjst be sure the names of teh data memebrs match the JSON property names
+ * OR use [JsonPropertyName] annotation to associate them
+ * 
+ ******************************************************************************/
+
+namespace moviesAPI.Models;
+
+public class Movie
+{
+    [JsonPropertyName("movieId")]
+    public int     MovieId     { get; set; }
+
+    [JsonPropertyName("title")]
+    public string  Title       { get; set; } = null!;
+
+    [JsonPropertyName("releaseYear")]
+    public int?    ReleaseYear { get; set; }
+
+    [JsonPropertyName("director")]
+    public string? Director    { get; set; }
+}
