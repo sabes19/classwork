2e46c3b846b1ed5d6f036f6920ff2bd7e6b58526
FFrank Fella <Frank@CyberAcademy.com>
2025-01-13 19:00:53 -0700

    Instructor Push 01/13/2025 19:00:52

--- Unit-9-Full-Stack-Angular/practice-assessment-8-FrankCyberFella-OLD/bookclub-frontend/src/app/bookClubMemberModel.ts
+++
@@ -1,7 +0,0 @@
-// interface allows us to share data description with different processes
-export interface bookClubMemberModel {
-    id        : string,
-    name      : string,
-    booksRead : number,
-    foundingMember : boolean
-}
